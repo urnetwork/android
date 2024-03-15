b07de1c5e646e6c9a2fda33e6ec1d0078a5edc5d
Brien Colwell <xcolwell@gmail.com>
2024-03-14 23:36:58 -0700

    git lfs migrate import

---
+++ app/app/build/generated/source/buildConfig/androidTest/play/debug/com/bringyour/network/test/BuildConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.bringyour.network.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bringyour.network.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "play";
+}
---
+++ app/app/build/generated/source/buildConfig/play/debug/com/bringyour/network/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.bringyour.network;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bringyour.network";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "play";
+  public static final int VERSION_CODE = 7126;
+  public static final String VERSION_NAME = "2024.3.14-outerwerld+1710479937511";
+}
