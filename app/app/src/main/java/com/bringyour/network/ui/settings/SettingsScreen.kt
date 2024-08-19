cb5da41471600ec2990eeb053b8fb181a8e9480d
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-08-19 14:30:34 +0900

    info icon with overlay

--- app/app/src/main/java/com/bringyour/network/ui/settings/SettingsScreen.kt
+++ app/app/src/main/java/com/bringyour/network/ui/settings/SettingsScreen.kt
@@ -2,8 +2,8 @@ package com.bringyour.network.ui.settings
 
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,7 +22,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -32,23 +31,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.lerp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.bringyour.network.ui.components.AccountSwitcher
-import com.bringyour.network.ui.components.LoginMode
+import com.bringyour.network.ui.components.InfoIconWithOverlay
 import com.bringyour.network.ui.components.URLinkText
 import com.bringyour.network.ui.components.URSwitch
 import com.bringyour.network.ui.components.URTextInputLabel
 import com.bringyour.network.ui.theme.Black
+import com.bringyour.network.ui.theme.BlueLight
 import com.bringyour.network.ui.theme.BlueMedium
 import com.bringyour.network.ui.theme.TopBarTitleTextStyle
 import com.bringyour.network.ui.theme.URNetworkTheme
@@ -103,12 +101,52 @@ fun SettingsScreen(
                     .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Row {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     Text(
                         "URnetwork member",
                         style = MaterialTheme.typography.bodyMedium,
                         color = Color.White
                     )
+
+                    Spacer(modifier = Modifier.width(2.dp))
+
+                    InfoIconWithOverlay() {
+                        Column() {
+
+                            Text(
+                                "Unlock even faster speeds and first dibs on new features.",
+                                style = MaterialTheme.typography.bodySmall,
+                                color = BlueLight
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth()
+                                    .clickable {
+                                        // todo - navigate to premium user flow
+                                    },
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    "Become a supporter",
+                                    style = TextStyle(
+                                        fontSize = 12.sp,
+                                        lineHeight = 20.sp,
+                                        fontWeight = FontWeight(700),
+                                        color = BlueLight,
+                                    )
+                                )
+                                Icon(imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                                    contentDescription = "Right Arrow",
+                                    tint = BlueLight,
+                                    modifier = Modifier.size(20.dp)
+                                )
+                            }
+                        }
+                    }
                 }
                 ClickableText(
                     text = AnnotatedString("Change"),
@@ -172,16 +210,18 @@ fun SettingsScreen(
                 modifier = Modifier
                     .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.Bottom
             ) {
 
-                Row {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     Text(
                         "Join the community on",
                         style = MaterialTheme.typography.bodyMedium,
                         color = Color.White
                     )
-                    Spacer(modifier = Modifier.width(2.dp))
+                    Spacer(modifier = Modifier.width(3.dp))
                     URLinkText(
                         text = "Discord",
                         url = discordInviteLink,
