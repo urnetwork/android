a0cd0f4d6d5f9fd7c639cc825d3c72bc33b13a01
Brien Colwell <xcolwell@gmail.com>
2024-09-09 00:58:31 -0700

    start on upgrade and unsuspend

---
+++ app/app/src/main/java/com/bringyour/network/StartReceiver.kt
@@ -0,0 +1,20 @@
+package com.bringyour.network
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+
+class StartReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            Intent.ACTION_BOOT_COMPLETED, Intent.ACTION_MY_PACKAGE_REPLACED, Intent.ACTION_MY_PACKAGE_UNSUSPENDED -> {
+                val app = context.applicationContext as MainApplication
+                if (app.isVpnRequestStart()) {
+                    app.startVpnService()
+                }
+            }
+        }
+    }
+}
