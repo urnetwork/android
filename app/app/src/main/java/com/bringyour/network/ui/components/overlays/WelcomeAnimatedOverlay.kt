ffa8395959ec77bbef0e0012bb7d40dae884fbda
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-09-18 16:49:14 +0900

    mask shrinks; enter btn and text area

--- app/app/src/main/java/com/bringyour/network/ui/components/overlays/WelcomeAnimatedOverlay.kt
+++ app/app/src/main/java/com/bringyour/network/ui/components/overlays/WelcomeAnimatedOverlay.kt
@@ -2,22 +2,30 @@ package com.bringyour.network.ui.components.overlays
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.ImageBitmap
@@ -25,11 +33,17 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.bringyour.network.R
+import com.bringyour.network.ui.components.ButtonStyle
+import com.bringyour.network.ui.components.URButton
+import com.bringyour.network.ui.components.buttonTextStyle
 import com.bringyour.network.ui.theme.Black
+import com.bringyour.network.ui.theme.URNetworkTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -38,19 +52,79 @@ fun WelcomeAnimatedOverlay(
     animateIn: Boolean
 ) {
 
-    var isVisible by remember { mutableStateOf(animateIn) }
+    var maskExpanded by remember { mutableStateOf(true) }
 
     LaunchedEffect(Unit) {
         launch {
             delay(2000)
-            isVisible = false
+
+            maskExpanded = false
+
         }
     }
+
+    val maskSize by animateDpAsState(targetValue = if (maskExpanded) 2000.dp else 248.dp,
+        label = "mask size"
+    )
+    val topPaddingSize by animateDpAsState(targetValue = if (maskExpanded) 0.dp else 122.dp,
+        label = "top padding size"
+    )
+
+
+    WelcomeAnimatedOverlay(
+        // isVisible = isVisible,
+        animateIn = animateIn,
+        maskExpanded = maskExpanded,
+        maskSize = maskSize,
+        topPaddingSize = topPaddingSize
+    )
+}
+
+@Composable
+fun WelcomeAnimatedOverlay(
+    // isVisible: Boolean,
+    animateIn: Boolean,
+    maskExpanded: Boolean,
+    maskSize: Dp,
+    topPaddingSize: Dp,
+) {
+
+    var isVisible by remember { mutableStateOf(animateIn) }
+    var isClosing by remember { mutableStateOf(false) }
+    var isBodyVisible by remember { mutableStateOf(false) }
 
     val context = LocalContext.current
     val backgroundBitmap: ImageBitmap = ImageBitmap.imageResource(context.resources, R.drawable.overlay_onboarding)
 
     val exitTransition = fadeOut()
+
+    LaunchedEffect(maskExpanded) {
+        if (!maskExpanded) {
+            delay(1000)
+            isBodyVisible = true
+        }
+    }
+
+    LaunchedEffect(isClosing) {
+
+        launch {
+
+            if (isClosing) {
+                isBodyVisible = false
+
+                delay(1000)
+
+                isVisible = false
+
+                delay(1000)
+
+                isClosing = false
+            }
+
+        }
+
+
+    }
 
 
     AnimatedVisibility(
@@ -74,7 +148,8 @@ fun WelcomeAnimatedOverlay(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(122.dp)
+                    // .height(122.dp)
+                    .height(topPaddingSize)
                     .background(Black)
             )
 
@@ -85,7 +160,7 @@ fun WelcomeAnimatedOverlay(
 
                 Box(
                     modifier = Modifier
-                        .height(256.dp)
+                        .height(maskSize)
                         .fillMaxWidth()
                         .weight(1f)
                         .background(Black)
@@ -96,12 +171,13 @@ fun WelcomeAnimatedOverlay(
                     contentDescription = "Connect Mask",
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .size(256.dp)
+                        // .size(256.dp)
+                        .size(maskSize)
                 )
 
                 Box(
                     modifier = Modifier
-                        .height(256.dp)
+                        .height(maskSize)
                         .fillMaxWidth()
                         .weight(1f)
                         .background(Black)
@@ -109,23 +185,71 @@ fun WelcomeAnimatedOverlay(
 
             }
 
-            Box(
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .weight(1f)
                     .background(Black)
-            )
+            ) {
+
+                AnimatedVisibility(
+                    visible = isBodyVisible,
+                    enter = fadeIn(),
+                    exit = fadeOut(),
+                ) {
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Spacer(modifier = Modifier.height(32.dp))
+                        Text(
+                            "Nicely done",
+                            style = MaterialTheme.typography.headlineMedium
+                        )
+                        Text(
+                            "Step into the internet as it should be.",
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.headlineLarge
+                        )
+                        Spacer(modifier = Modifier.height(48.dp))
+                        URButton(
+                            onClick = {
+                                // isVisible = false
+                                isClosing = true
+                            },
+                            style = ButtonStyle.OUTLINE
+                        ) { buttonTextStyle ->
+                            Row(
+                                modifier = Modifier
+                                    .padding(horizontal = 48.dp),
+                            ) {
+                                Text(
+                                    "Enter",
+                                    style = buttonTextStyle
+                                )
+                            }
+                        }
+                    }
+                }
+            }
 
         }
-
     }
-
 }
 
 @Preview
 @Composable
 private fun WelcomeAnimatedOverlayPreview() {
-    WelcomeAnimatedOverlay(
-        animateIn = true
-    )
+    URNetworkTheme {
+        WelcomeAnimatedOverlay(
+            animateIn = true,
+            // isVisible = true,
+            maskExpanded = false,
+            maskSize = 248.dp,
+            topPaddingSize = 122.dp
+        )
+    }
 }
