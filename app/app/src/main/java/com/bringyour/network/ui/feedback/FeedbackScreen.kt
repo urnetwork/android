57c59ee4256168e63a8b220e1dd37f7b34aa5a68
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2025-02-27 09:04:46 +0900

    adds in discord link

--- app/app/src/main/java/com/bringyour/network/ui/feedback/FeedbackScreen.kt
+++ app/app/src/main/java/com/bringyour/network/ui/feedback/FeedbackScreen.kt
@@ -1,6 +1,7 @@
 package com.bringyour.network.ui.feedback
 
 import android.content.res.Configuration
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -22,24 +23,33 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.bringyour.network.R
 import com.bringyour.network.ui.components.URButton
-import com.bringyour.network.ui.components.URLinkText
 import com.bringyour.network.ui.components.URTextInput
 import com.bringyour.network.ui.components.overlays.OverlayMode
 import com.bringyour.network.ui.shared.viewmodels.OverlayViewModel
+import com.bringyour.network.ui.theme.Pink
 import com.bringyour.network.ui.theme.URNetworkTheme
 import com.bringyour.network.utils.isTablet
 import com.bringyour.network.utils.isTv
@@ -163,7 +173,6 @@ fun FeedbackScreen(
                 modifier = Modifier
                     .width(512.dp)
                     .fillMaxHeight()
-                    // .padding(16.dp)
                     .padding(horizontal = 16.dp)
                     .padding(innerPadding)
                     .imePadding(),
@@ -194,7 +203,38 @@ private fun FeedbackForm(
     isSendEnabled: Boolean,
 ) {
 
+    val supportUrl = "https://discord.com/invite/RUNZXMwPRK"
+
+    val uriHandler = LocalUriHandler.current
+
+    val feedbackAnnotatedString = buildAnnotatedString {
+        withStyle(
+            style = MaterialTheme.typography.bodyLarge.toSpanStyle()
+                .copy(color = Color.White)
+        ) {
+            append("Send us your feedback directly or ")
+
+            pushStringAnnotation(
+                tag = "URL",
+                annotation = supportUrl
+            )
+            withStyle(
+                style = SpanStyle(
+                    color = Pink
+                )
+            ) {
+                append("join our Discord ")
+            }
+
+            append("for direct support.")
+
+            pop()
+        }
+    }
+
     val keyboardController = LocalSoftwareKeyboardController.current
+
+    var layoutResult by remember { mutableStateOf<TextLayoutResult?>(null) }
 
     Column {
         Text(
@@ -204,15 +244,25 @@ private fun FeedbackForm(
 
         Spacer(modifier = Modifier.height(64.dp))
 
-        Text("Send us your feedback directly or take a look at", style = MaterialTheme.typography.bodyLarge, color = Color.White)
+        Text(
+            text = feedbackAnnotatedString,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.pointerInput(Unit) {
+                detectTapGestures { offset ->
+                    layoutResult?.let { layoutResult ->
+                        val position = layoutResult.getOffsetForPosition(offset)
+                        val annotation = feedbackAnnotatedString
+                            .getStringAnnotations(position, position)
+                            .firstOrNull()
 
-        Row(
-            verticalAlignment = Alignment.Bottom
-        ) {
-            Text("our", style = MaterialTheme.typography.bodyLarge, color = Color.White)
-            Spacer(modifier = Modifier.width(1.dp))
-            URLinkText(text = "support resources", url = "https://support.bringyour.com/en/")
-        }
+                        if (annotation?.tag == "URL") {
+                            uriHandler.openUri(annotation.item)
+                        }
+                    }
+                }
+            },
+            onTextLayout = { layoutResult = it }
+        )
 
         Spacer(modifier = Modifier.height(32.dp))
 
