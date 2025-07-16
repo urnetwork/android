f2e9822a04d97c87219d32778cdda108f460c495
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2025-07-16 15:07:22 +0900

    fixes spanish language formatting error

--- app/app/src/main/java/com/bringyour/network/ui/settings/updateReferralNetworkBottomSheet/UpdateReferralNetworkBottomSheet.kt
+++ app/app/src/main/java/com/bringyour/network/ui/settings/updateReferralNetworkBottomSheet/UpdateReferralNetworkBottomSheet.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.bringyour.network.R
-import com.bringyour.network.ui.components.URButton
 import com.bringyour.network.ui.components.URTextInput
 import com.bringyour.network.ui.theme.ppNeueBitBold
 
@@ -70,7 +69,6 @@ fun UpdateReferralNetworkBottomSheet(
                 )
             },
             onDismissRequest = {
-                // onDismissRequest()
                 viewModel.setDisplayUnlinkAlert(false)
             },
             confirmButton = {
