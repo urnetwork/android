c5194410147703c683e9d68f62488c1e6549bf06
Brien Colwell <xcolwell@gmail.com>
2025-10-18 02:16:21 -0500

    ethos: update

--- app/app/src/ethos_dapp/java/com/bringyour/network/ui/upgrade/UpgradeScreen.kt
+++ app/app/src/ethos_dapp/java/com/bringyour/network/ui/upgrade/UpgradeScreen.kt
@@ -1,24 +1,21 @@
 package com.bringyour.network.ui.upgrade
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.bringyour.network.ui.shared.viewmodels.OverlayViewModel
 import com.bringyour.network.ui.shared.viewmodels.PlanViewModel
-import com.bringyour.network.ui.shared.viewmodels.StripePaymentIntentViewModel
 
 @Composable
 fun UpgradeScreen(
     navController: NavHostController,
     planViewModel: PlanViewModel,
-    overlayViewModel: OverlayViewModel,
     setPendingSolanaSubscriptionReference: (String) -> Unit,
     createSolanaPaymentIntent: (
         reference: String,
         onSuccess: () -> Unit,
         onError: () -> Unit
     ) -> Unit,
-    pollSubscriptionBalance: () -> Unit
+    onStripePaymentSuccess: () -> Unit,
+    isCheckingSolanaTransaction: Boolean
 ) {
 
     /**
@@ -27,10 +24,10 @@ fun UpgradeScreen(
     UpgradePlanAlt(
         navController,
         planViewModel,
-        overlayViewModel,
         setPendingSolanaSubscriptionReference,
         createSolanaPaymentIntent,
-        pollSubscriptionBalance
+        onStripePaymentSuccess = onStripePaymentSuccess,
+        isCheckingSolanaTransaction = isCheckingSolanaTransaction
     )
 
 }
