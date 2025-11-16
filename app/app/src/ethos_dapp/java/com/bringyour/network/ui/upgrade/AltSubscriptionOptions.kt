1ae2926b6e8edb4bdd29d6861689c01533a92e0c
Brien Colwell <xcolwell@gmail.com>
2025-11-16 08:06:50 -0600

    build: update

--- app/app/src/ethos_dapp/java/com/bringyour/network/ui/upgrade/AltSubscriptionOptions.kt
+++ app/app/src/ethos_dapp/java/com/bringyour/network/ui/upgrade/AltSubscriptionOptions.kt
@@ -11,39 +11,30 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.bringyour.network.R
 import com.bringyour.network.ui.components.URButton
-import com.bringyour.network.ui.upgrade.StripePaymentIntentViewModel
 import com.bringyour.network.ui.theme.OffBlack
 import com.bringyour.network.ui.theme.TextMuted
-import com.bringyour.network.ui.theme.URNetworkTheme
 import com.bringyour.network.ui.theme.Yellow
 import com.stripe.android.PaymentConfiguration
-import com.stripe.android.paymentsheet.ExperimentalCustomerSessionApi
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheetResult
 
-@OptIn(ExperimentalCustomerSessionApi::class)
+//@OptIn(ExperimentalCustomerSessionApi::class)
 @Composable
 fun AltSubscriptionOptions(
     onStripePaymentSuccess: () -> Unit,
